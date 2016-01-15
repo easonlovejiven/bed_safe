bd672f6083823e3ba26eec78adcfa9d8980aa3bb
yuanjianhe <jianhe.yuan@weizhuli.com>
2016-01-15 15:13:26 +0800

    入库时间查询bug

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,7 +5,6 @@ class Product < ActiveRecord::Base
   TOP_AND_DOWN = { 0 => "已下架", 1 => "未下架" } # 默认是未下架
 
   # 按照日期进行搜索
-
-  scope :created_at_gte, -> (date_str) { date_str.present? ? where("created_at >= ?", Date.parse(date_str)) : all}
-  scope :created_at_lte, -> (date_str) { date_str.present? ? where("created_at <= ?", Date.parse(date_str) + 1) : all}
+  scope :created_at_gte, -> (date_str) { date_str.present? ? where('created_at >= ?', Date.parse(date_str)) : all }
+  scope :created_at_lte, -> (date_str) { date_str.present? ? where('created_at <= ?', Date.parse(date_str) + 1) : all }
 end
