b4aea6fe3337810868e518bbd9b79132f5bd4bc9
yuanjianhe <jianhe.yuan@weizhuli.com>
2016-01-27 12:58:26 +0800

    validate_method_by_description

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ActiveRecord::Base
 	belongs_to :type
 	belongs_to :user
+  validate :describtion_validates
 
 	# 有命名空间
 	# belongs_to :sender_staffer, class_name: "Admin::Staffer", foreign_key: :sender_staffer_id #生产者
@@ -16,6 +17,24 @@ class Product < ActiveRecord::Base
   # 按照日期进行搜索
   scope :created_at_gte, -> (date_str) { date_str.present? ? where('created_at >= ?', Date.parse(date_str)) : all }
   scope :created_at_lte, -> (date_str) { date_str.present? ? where('created_at <= ?', Date.parse(date_str) + 1) : all }
+
+  # 对录入产品信息做个验证
+  def describtion_validates
+    self.errors.add(:describtion, "产品描述输入有误，请重新录入！") if self.describtion.present? && !self.describtion.include?("值得信任！")
+  end
+
+  # 验证手机号是否输入有误
+  # validate :mebile_validates
+  # def mebile_validates
+  #   mobile_errors = []
+  #   if self.receive_tel.present?
+  #     mobiles = self.receive_tel.split("\r\n") 录入时必须换行
+  #     mobiles.uniq.each do |mobile|
+  #       mobile_errors << /^1[0-9]{10}$/.match(mobile)
+  #     end
+  #     self.errors.add(:receive_tel, "手机格式输入有误，请重新录入！") if mobile_errors.include?(nil)
+  #   end
+  # end
 
   # scope 用法
   #scope :area, -> {where("id > 2")} #scope 没有参数的
@@ -33,7 +52,7 @@ class Product < ActiveRecord::Base
 
   # before_save :change_count, if: :discount  条件回调
   # def change_count
-  #   # 如果是有折扣的话，然后做另外的处理(其它回调方法的用法都一样)
+  #   # 如果是有折扣的话，然后做另外的处理(其它回调方法 的用法都一样)
   # end
 
   # after_initialize :new_product  初始化回调
@@ -42,9 +61,9 @@ class Product < ActiveRecord::Base
   # end
 
   # 在这儿做属性的处理的话，在查询的时候，读取对象.属性的时候会调用这个方法，默认会去找这个属性
-  def price
-    self.price.to_f
-  end
+  # def price
+  #   self.price.to_f
+  # end
 
 
 end
